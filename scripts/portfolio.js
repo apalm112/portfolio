92c448ae71d5f5bdb62317ea49638c082a375114
apalm112 <apalm112@gmail.com>
2016-04-11 21:42:01 -0700

    Finished code for contructor function & prototype function.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -5,5 +5,20 @@ function Project (repos) {
   this.title = repos.title;
   this.updatedOn = repos.updatedOn;
   this.body = repos.body;
-
 }
+
+Project.prototype.toHtml = function () {
+  var $newProject = $('article.template').clone();
+
+  $newProject.attr('data-title', this.title);
+  $newProject.attr('data-updatedOn', this.updatedOn);
+  $newProject.attr('data-body', this.body);
+
+  $newArticle.find('time[pubdate]').attr('title', this.updatedOn);
+  $newArticle.find('section.article-body').html(this.body);
+  $newArticle.find('header h2').text(this.title);
+
+  $newArticle.removeClass('template');
+
+  return $newArticle;
+};
