1ae68bcd1f92d46a0bcc951d5c56863fa69b5df3
apalm112 <apalm112@gmail.com>
2016-04-13 14:47:39 -0700

    Added repo info to projects.js, removed some styling from base.css, added projects.js to index.html & started code for template append to DOM.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,3 +1,17 @@
 var projects = [
-
+  {
+    title: 'vote tracker',
+    updatedOn: '12-10-2015',
+    course: 'Code 101'
+  },
+  {
+    title: 'You Tube API',
+    updatedOn: '1-29-2016',
+    course: 'Code 301'
+  },
+  {
+    title: 'Portfolio',
+    updatedOn: '4-13-2016',
+    course: 'Code 301'
+  }
 ];
