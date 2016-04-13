940d72ae357b82e34d7808350739432d42853471
apalm112 <apalm112@gmail.com>
2016-04-12 21:29:25 -0700

    Added minor style changes, an About section to index.html & main-nav function.

---
+++ scripts/portfolioView.js
@@ -0,0 +1,9 @@
+var portfolioView = {};
+
+portfolioView.handleMainNav = function() {
+  $('.main-nav').on('click', '.tab', function(e) {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+  $('.main-nav .tab:first').click();
+};
