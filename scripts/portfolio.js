5fa7dd8ae3c4a28832654a04722ea44c06e5e13b
apalm112 <apalm112@gmail.com>
2016-04-14 15:03:20 -0700

    Added handlebars script for html head.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,18 +1,18 @@
-var projects = [];  // i don't think i need this array here
+var githubRepos = [];  // i don't think i need this array here
 
 function Project (repos) {
   // constructor function for github repos to be displayed
   this.title = repos.title;
   this.updatedOn = repos.updatedOn;
-  this.body = repos.body;
+  this.course = repos.course;
 }
 
 Project.prototype.toHtml = function () {
-  var $newProject = $('article.template').clone();
+/*  var $newProject = $('article.template').clone();
 
   $newProject.attr('data-title', this.title);
   $newProject.attr('data-updatedOn', this.updatedOn);
-  $newProject.attr('data-body', this.body);
+
 
   $newArticle.find('time[pubdate]').attr('title', this.updatedOn);
   $newArticle.find('section.article-body').html(this.body);
@@ -20,9 +20,18 @@ Project.prototype.toHtml = function () {
 
   $newArticle.removeClass('template');
 
-  return $newArticle;
+  return $newArticle;*/
+
+  var $source = $('#project-template').html();
+  var template = Handlebars.compile($source);
+
+  return template(this);
 };
 
-projects.forEach(function(){
-  $('#repos').append(toHtml);
+projects.forEach(function(proj) {
+  githubRepos.push(new Project(proj));
+});
+
+githubRepos.forEach(function(a) {
+  $('.project-placeholder').append(a.toHtml);
 });
