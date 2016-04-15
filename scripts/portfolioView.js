d3da962cb432251e06a7af1f52390b1019ad0a55
apalm112 <apalm112@gmail.com>
2016-04-15 16:27:08 -0700

    Started code for adding project form.

--- scripts/portfolioView.js
+++ scripts/portfolioView.js
@@ -12,3 +12,26 @@ portfolioView.handleMainNav = function() {
 $(function() {
   portfolioView.handleMainNav();
 });
+
+portfolioView.initNewProjectPage = function() {
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('#project-json').on('focus', function() {
+    $(this).select();
+  });
+
+  $('#new-form').on('change', 'input',
+'textarea', portfolioView.create);
+};
+
+portfolioView.create = function() {
+  $('#project-preview').empty();
+
+  var formProject = new Project( {
+    title: $('project-title').val(),
+    updatedOn: $('#project-updatedOn:checked').length ? new Date() : null
+  });
+
+
+
+};
