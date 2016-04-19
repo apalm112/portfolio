ea6e6d03aecb85fbf324381f82a5eaaa5b224cc9
apalm112 <apalm112@gmail.com>
2016-04-18 20:42:53 -0700

    Changed githubRepos array into Project property, started code on Project.fetchAll function to populate project.json data with .ajax call.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,11 +1,11 @@
-var githubRepos = [];
-
 function Project (repos) {
   // constructor function for github repos to be displayed
   this.title = repos.title;
   this.updatedOn = repos.updatedOn;
   this.course = repos.course;
 }
+
+Project.all = []
 
 Project.prototype.toHtml = function () {
   var $source = $('#project-template').html();
@@ -20,3 +20,17 @@ projects.forEach(function(proj) {
 githubRepos.forEach(function(a) {
   $('#project-placeholder').append(a.toHtml());
 });
+
+Project.fetchAll = function() {
+  $.ajax({
+    type: 'HEAD',
+    url: 'data/projects.json',
+    success: function (data, message, xhr) {
+      var eTag = xhr.getResponseHeader('eTag');
+      if (eTag === localStorage.eTag) {
+        Project.
+      };
+
+    }
+  });
+};
