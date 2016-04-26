8e05ae17ee32c2c5e7af0517176b5827a8c8df8a
apalm112 <apalm112@gmail.com>
2016-04-25 19:48:58 -0700

    Added aboutController to index.html & the aboutController.index method for the about section to route.

---
+++ scripts/aboutController.js
@@ -0,0 +1,12 @@
+(function(module) {
+  var aboutController = {};
+
+  aboutController.index = function() {
+  /*  $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();*/
+    $('#projects').hide();
+    $('#about').show();
+  };
+
+  module.aboutController = aboutController;
+})(window);
