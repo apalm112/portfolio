39c390f8c5c5d221e836052bbbbfa920a82a709d
apalm112 <apalm112@gmail.com>
2016-04-27 13:06:57 -0700

    Changed .ajax() to a .get() call, to reflect the Github Token now being an ENV.

--- scripts/model/repo.js
+++ scripts/model/repo.js
@@ -4,18 +4,12 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/apalm112/repos' +
-           '?per_page=5' +
-           '&sort=updated',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + githubToken},
-      success: function(data) {
+    $.get('https://api.github.com/users/apalm112/repos' + '?per_page=5' + '&sort=updated')
+      .done(function(data) {
         repos.all = data;
         console.log('Here is yo ajax response data: ', data);
-      }
-    });
-    Project.loadAll(repos.data); //  Wrong place for callback? (callback)
+      });
+    callback(repos.data); //  Wrong place for callback?
   };
 
   repos.with = function(attr) {
