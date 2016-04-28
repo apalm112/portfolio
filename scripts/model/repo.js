7cec70866dc65045828033ea2ef36c3b60a1f595
apalm112 <apalm112@gmail.com>
2016-04-28 15:34:46 -0700

    commented out debugger()

--- scripts/model/repo.js
+++ scripts/model/repo.js
@@ -4,10 +4,13 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.get('https://api.github.com/users/apalm112/repos' + '?per_page=5' + '&sort=updated')
+    $.get('https://api.github.com/users/apalm112/repos' +
+          '?per_page=5' +
+          '&sort=updated')
       .done(function(data) {
+      //  debugger;
         repos.all = data;
-        console.log('Here is yo ajax response data: ', data);
+        console.log('Here is yo .get() response data: ', data);
       });
     //repoView.index();
   //  portfolioView.initIndexPage(); <---this callback literally does nothing.
