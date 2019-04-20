20d48311f3ce97ae66532b34c1c5c2863401a9ad
Adam <apalm112@gmail.com>
2019-04-20 12:20:25 -0400

    Modified the project descriptions to fit better.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
-/*	This object shall contain 12 projects & their data in order to display a card for each project.
-*/
+//	This object shall contain 12 projects & their data in order to display a card for each project.
 exports.projects = [
 	{
 		name: 'WA School Immunization Rates',
@@ -14,14 +13,14 @@ exports.projects = [
 		img: 'proj-11.png',
 		repo_url: 'https://github.com/apalm112/FSJS-Course-Rating-API-Express',
 		deploy: '',
-		description: 'A REST API using Express. The API will provide a way for users to review educational courses.'
+		description: 'A REST API using Express that let users CRUD items from a database.'
 	},
 	{
 		name: 'Library Manager',
 		img: 'proj-10.png',
 		repo_url: 'https://github.com/apalm112/FSJS-Build-A-Library-Manager',
 		deploy: '',
-		description: 'Implement a library manager like dynamic website using Express, Pug, and SQL.'
+		description: 'A library manager CRUD application using JS, Node.js, Express, Pug, and the SQL ORM Sequelize.'
 	},
 	{
 		name: 'React Gallery App',
@@ -49,41 +48,41 @@ exports.projects = [
 		img: 'proj-6.png',
 		repo_url: 'https://github.com/apalm112/Project6_Content_Scraper',
 		deploy: '',
-		description: ' A Node.js command line application that scrapes the latest prices and save the data.'
+		description: 'A Node.js command line application that scrapes the latest prices from a web site & saves the data.'
 	},
 	{
 		name: 'Public API Employee Directory',
 		img: 'proj-5.png',
 		repo_url: 'https://github.com/apalm112/FSJS-Use-Public-API-to-Create-Employee-Directory',
 		deploy: 'https://apalm112.github.io/FSJS-Use-Public-API-to-Create-Employee-Directory/',
-		description: 'Uses the Random User Generator API to create a working example of a directory.'
+		description: 'Uses the Random User Generator API to create a working example of an employee directory.'
 	},
 	{
 		name: 'Tic-Tac-Toe',
 		img: 'proj-4.png',
 		repo_url: 'https://github.com/apalm112/FSJS-Tic-Tac-Toe',
 		deploy: 'https://apalm112.github.io/FSJS-Tic-Tac-Toe/',
-		description: 'A JavaScript powered two player Tic-Tac-Toe game.  Created the JS to run it given provided HTML & CSS files.'
+		description: 'A JavaScript two player Tic-Tac-Toe game. Coded the JS to run it given provided HTML & CSS files.'
 	},
 	{
 		name: 'Interactive Form',
 		img: 'proj-3.png',
 		repo_url: 'https://github.com/apalm112/FSJS-Interactive-Form',
 		deploy: 'https://apalm112.github.io/FSJS-Interactive-Form/',
-		description: 'Project makes user interactivity possible to a provided form by adding JavaScript to it.'
+		description: 'Project provides user interactivity to a provided form by adding JavaScript to it.'
 	},
 	{
 		name: 'Pagination & Content Filter',
 		img: 'proj-2.png',
 		repo_url: 'https://github.com/apalm112/FSJS-Pagination',
 		deploy: 'https://apalm112.github.io/FSJS-Pagination/',
-		description: 'Project adds pagination & search bar features that allow users to search.'
+		description: 'Project dynamically adds pagination & search bar features that allow users to search.'
 	},
 	{
 		name: 'Random Quote Generator',
 		img: 'proj-1.png',
 		repo_url: 'https://github.com/apalm112/FSJS-Random-Quote-Generator',
 		deploy: 'https://apalm112.github.io/FSJS-Random-Quote-Generator/',
-		description: 'An app that displays random famous quotes each time a button is clicked.'
+		description: 'Static site that uses vanilla JS to display random quotes each time a button is clicked.'
 	}
 ];
